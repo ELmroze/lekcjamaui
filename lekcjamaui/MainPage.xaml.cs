8f17bebd0b7f086daf264a0eefd66b21a718798d
Emil Skrzydeł <mrolere@Gmail.com>
2024-09-16 20:06:35 +0200

    Add project files.

---
+++ lekcjamaui/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace lekcjamaui
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private bool szukanie_malpy(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(text[i] == '@')return true;
+            }
+            return false;
+        }
+
+        private void zatwierdz(System.Object sender, System.EventArgs e)
+        {
+            if (mail.Text.Length > 0)
+            {
+                if (szukanie_malpy(mail.Text))
+                {
+                    if (password.Text == password2.Text)
+                    {
+                        odpowiedz.Text = "Witaj " + mail.Text;
+                    }
+                    else
+                    {
+                        odpowiedz.Text = "Hasła się różnią";
+                    }
+                }
+                else
+                {
+                    odpowiedz.Text = "Nieprawidłowy adres e-mail";
+                }
+            }
+        }
+    }
+
+}
---
+++ lekcjamaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace lekcjamaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
